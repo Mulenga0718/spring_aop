f05772847ffa8a72abe06c59601cae6856cbee9e
PC-03 <PC-03@DESKTOP-IU1TOGH>
2022-06-14 18:42:13 +0900

    spring_aop

---
+++ spring_AOP/src/main/java/com/spring/pointcut/BehaviorImpl.java
@@ -0,0 +1,53 @@
+package com.spring.pointcut;
+
+public class BehaviorImpl implements Behavior {
+
+	@Override
+	public void 잠자기() {
+		// TODO Auto-generated method stub
+		System.out.println("쿨쿨~~잠을 잡니다.");
+	}
+
+	@Override
+	public void 공부하기() {
+		// TODO Auto-generated method stub
+
+		System.out.println("열심히 공부를 합니다.");
+	}
+
+	@Override
+	public void 밥먹기() {
+		// TODO Auto-generated method stub
+
+		System.out.println("냠냠...쩝쩝... 다흘리고 먹어요...");
+	}
+
+	@Override
+	public void 데이트() {
+		// TODO Auto-generated method stub
+
+		System.out.println("귀찮아... 데이트...");
+	}
+
+	@Override
+	public void 운동() {
+		// TODO Auto-generated method stub
+
+		System.out.println("근손실을 막기 위해서!!");
+	}
+
+	@Override
+	public void 놀기() {
+		// TODO Auto-generated method stub
+
+		System.out.println("놀자~~~~");
+	}
+
+	@Override
+	public void 정신수양() {
+		// TODO Auto-generated method stub
+
+		System.out.println("띵...");
+	}
+
+}
